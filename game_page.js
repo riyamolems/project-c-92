c34a92a1310c1cc0e69c5bd2e6e17c32c38ff45e
riyamolems <73664122+riyamolems@users.noreply.github.com>
2021-03-29 22:39:55 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -42,13 +42,13 @@ function check()
 	{
 		if(answer_turn == "player1")
 		{
-			update_player1_score = player1_score +1;
-		    document.getElementById("player1_score").innerHTML = update_player1_score;
+			player1_score = player1_score +1;
+		    document.getElementById("player1_score").innerHTML = player1_score;
 		}
 		else 
 		{
-			update_player2_score = player2_score +1;
-		    document.getElementById("player2_score").innerHTML = update_player2_score;
+			player2_score = player2_score +1;
+		    document.getElementById("player2_score").innerHTML = player2_score;
 		}
 	}
 	if(question_turn == "player1")
